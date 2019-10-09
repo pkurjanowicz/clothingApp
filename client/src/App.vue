1ef2778255aa174cf1a155d86051368494783d64
Peter <pkurjanowicz5@gmail.com>
2019-10-08 18:32:38 -0600

    removed fodder code and spun up virtual environment pipenv

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <appname welcome="Clothing Match"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appname from './components/nav-bar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appname
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ client/src/components/nav-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ welcome }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appname',
+  props: {
+    welcome: String
+  }
+}
+</script>
+
+<style>
+</style>
